2303ceab64f0f8b42676102998c1d3f56c0c1544
misha.gavela <Arfey17.mg@gmail.com>
2019-05-23 01:25:06 +0300

    fix: corrected setup.py settings

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-"""The setup script."""
+import sys
 
 from setuptools import setup, find_packages
+
+PY_VER = sys.version_info
+
+
+if not PY_VER >= (3, 5):
+    raise RuntimeError("aiohttp_admin doesn't support Python earlier than 3.5")
 
 with open('README.rst') as readme_file:
     readme = readme_file.read()
@@ -11,13 +14,16 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [ ]
+requirements = []
 
 setup_requirements = ['pytest-runner', ]
 
 test_requirements = ['pytest', ]
 
 setup(
+    name='aiohttp_admin2',
+    description="Admin interface for aiohttp application.",
+    long_description=readme + '\n\n' + history,
     author="Mykhailo Havelia",
     author_email='misha.gavela@gmail.com',
     classifiers=[
@@ -25,26 +31,25 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Framework :: AsyncIO',
+        'Operating System :: POSIX',
+        'Environment :: Web Environment',
     ],
-    description="Python Boilerplate contains all the boilerplate you need to create a Python package.",
-    install_requires=requirements,
-    license="Apache Software License 2.0",
-    long_description=readme + '\n\n' + history,
-    include_package_data=True,
     keywords='aiohttp_admin2',
-    name='aiohttp_admin2',
+    license="Apache Software License 2.0",
+    install_requires=requirements,
+    include_package_data=True,
     packages=find_packages(include=['aiohttp_admin2']),
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/Arfey/aiohttp_admin2',
     version='0.1.0',
+    platforms=['POSIX'],
     zip_safe=False,
 )
