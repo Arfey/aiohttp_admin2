ede1d48168d89e0f4d17a09fe0d42bd34cabb85e
misha.gavela <Arfey17.mg@gmail.com>
2019-06-27 01:18:21 +0300

    docs: added docs for template and corrected doc string

--- aiohttp_admin2/setup.py
+++ aiohttp_admin2/setup.py
@@ -1,9 +1,5 @@
-from typing import (
-    Any,
-    Dict,
-)
-
 from aiohttp import web
+from aiohttp_jinja2 import APP_KEY
 
 from .base import Admin
 
@@ -14,8 +10,10 @@ def setup(
     app: web.Application,
     *,
     admin_class=Admin,
+    jinja_app_key: str = APP_KEY
 ) -> None:
     """
-    Docs
+    This is a main function for initialize an admin interface for the given
+    application.
     """
-    admin_class(app).setup_admin_application()
+    admin_class(app).setup_admin_application(jinja_app_key=jinja_app_key)
--- docs/templates.rst
+++ docs/templates.rst
@@ -5,3 +5,10 @@ For generate pages `aiohttp_admin` using `jinja2`. You can rewrite native
 templates for `aiohttp_admin`. For that you should create `admin` directory
 into templates's directory for the `jinja2` and create your template with name
 of template witch you want to rewrite.
+
+If you setup `aiohttp_jinja2` with not default `jinja_app_key`  argument then
+you should initialize admin interface with your `jinja_app_key` argument.
+
+.. code-block:: python
+
+    aiohttp_admin.setup(app, jinja_app_key='my_jinja_value')
