4f43e2d5f5db3a5159bffdb83c8b2f15377c91b0
Arfey <Arfey17.mg@gmail.com>
2025-05-19 16:28:35 +0300

    fixed dependencies

--- tests/manager_fixtures.py
+++ tests/manager_fixtures.py
@@ -1,13 +1,24 @@
 import asyncio
+import contextvars
 
+import aiomysql.sa
+import aiopg.sa
 import pytest
 import sqlalchemy as sa
+from pytest_asyncio import is_async_test
 from sqlalchemy.schema import (
     CreateTable,
     DropTable,
 )
-import aiopg.sa
-import aiomysql.sa
+
+from aiohttp_admin2.resources import (
+    DictResource,
+    PostgresResource,
+)
+from aiohttp_admin2.resources.mysql_resource.mysql_resource import (
+    MySqlResource,
+)
+
 # wait to fix problem with
 # ImportError: Error importing plugin "tests.manager_fixtures": cannot import name 'coroutine' from 'asyncio'
 # solution: update to motor ^3.0.0
@@ -15,11 +26,6 @@ import aiomysql.sa
 # from umongo.frameworks import MotorAsyncIOInstance
 # from umongo import Document, fields
 
-from aiohttp_admin2.resources import (
-    PostgresResource,
-    DictResource,
-)
-from aiohttp_admin2.resources.mysql_resource.mysql_resource import MySqlResource
 # from aiohttp_admin2.resources.mongo_resource.mongo_resource import MongoResource
 
 
@@ -36,12 +42,36 @@ table = sa.Table('table', sa.MetaData(),
     sa.Column('val2', sa.String(255), nullable=True),
 )
 
+test_context = contextvars.Context()
 
-@pytest.yield_fixture(scope='session')
-def event_loop():
-    loop = asyncio.get_event_loop_policy().new_event_loop()
-    loop.set_debug(True)
-    yield loop
+
+def pytest_collection_modifyitems(items):
+    # fix how to use the same loop for each test
+    # https://pytest-asyncio.readthedocs.io/en/v0.24.0/how-to-guides/run_session_tests_in_same_loop.html
+    pytest_asyncio_tests = (item for item in items if is_async_test(item))
+    session_scope_marker = pytest.mark.asyncio(loop_scope="session")
+    for async_test in pytest_asyncio_tests:
+        async_test.add_marker(session_scope_marker, append=False)
+
+
+@pytest.fixture(scope="session")
+def event_loop_policy():
+    # fix how to use the same context for each test
+    # https://github.com/pytest-dev/pytest-asyncio/issues/127#issuecomment-2062158881
+    class CustomEventLoopPolicy(asyncio.DefaultEventLoopPolicy):
+        def task_factory(self, loop, coroutine):
+            return asyncio.Task(coroutine, loop=loop, context=test_context)
+
+        def new_event_loop(self):
+            loop = self._loop_factory()
+            loop.set_task_factory(self.task_factory)
+            return loop
+
+    policy = CustomEventLoopPolicy()
+
+    yield policy
+
+    policy.get_event_loop().close()
 
 
 @pytest.fixture(scope='session')
@@ -75,7 +105,6 @@ async def mongo_resource(mongo):
 
 @pytest.fixture(scope='session')
 async def mysql_resource(mysql):
-    asyncio.get_event_loop()
     async with aiomysql.sa.create_engine(**mysql) as engine:
         async with engine.acquire() as conn:
             await conn.execute(CreateTable(table))
